8045680893f49cc63462161bfb7d06ca94cc128f
Guillermo Arellano <guillermo.arellanojr@gmail.com>
2017-12-26 14:37:41 -0600

    Initial setup of ReposListComponent

--- src/app/repos/repos-list.component.ts
+++ src/app/repos/repos-list.component.ts
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./repos-list.component.scss']
 })
 export class ReposListComponent implements OnInit {
+  repos = ["repo_name1", "repo_name2", "repo_name3", "repo_name4", "repo_name5", "repo_name6", "repo_name7", "repo_name8", "repo_name9", "repo_name10"];
+  selectedRepo: string = "";
+  errorMessage: string = "";
 
   constructor() { }
 
