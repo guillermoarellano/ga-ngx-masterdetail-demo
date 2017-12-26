d0bb84da2d44700340fca33000fd2a99960ddf88
Guillermo Arellano <guillermo.arellanojr@gmail.com>
2017-12-26 14:18:50 -0600

    Added header text for the sidenav and sidenav main content areas

--- src/app/layout/sidenav.component.ts
+++ src/app/layout/sidenav.component.ts
@@ -11,14 +11,10 @@ import { ChangeDetectorRef, Component, OnInit, OnDestroy } from '@angular/core';
 export class SidenavComponent implements OnInit, OnDestroy {
   mobileQuery: MediaQueryList;
 
-  fillerNav = Array(50).fill(0).map((_, i) => `Nav Item ${i + 1}`);
+  fillerNav = Array(5).fill(0).map((_, i) => `Nav Item ${i + 1}`);
 
-  fillerContent = Array(50).fill(0).map(() =>
-      `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-       labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-       laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
-       voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-       cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`);
+  fillerContent = Array(9).fill(0).map(() =>
+      `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod`);
 
   private _mobileQueryListener: () => void;
 
