2e2705af7b67505814e13a11a3ed789ea4f79c85
Guillermo Arellano <guillermo.arellanojr@gmail.com>
2018-02-02 12:32:04 +0100

    Imported all the necessary modules and providers for Sidenav component to render

--- src/app/layout/sidenav.component.spec.ts
+++ src/app/layout/sidenav.component.spec.ts
@@ -1,6 +1,22 @@
+// the Jasmine libraries
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
+// the necessary modules for component to render
+import { 
+  MatIconModule,
+  MatSidenavModule,
+  MatToolbarModule,
+  MatListModule,
+} from '@angular/material';
+import {LayoutModule} from '@angular/cdk/layout';
+
+// the component itself and providers
 import { SidenavComponent } from './sidenav.component';
+import { ReposListComponent } from '../repos/repos-list.component';
+import { ReposService } from '../shared/repos.service';
+import { RepoLanguagesComponent } from '../repos/repo-languages.component';
 
 describe('SidenavComponent', () => {
   let component: SidenavComponent;
@@ -8,7 +24,23 @@ describe('SidenavComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ SidenavComponent ]
+      declarations: [
+        SidenavComponent,
+        ReposListComponent,
+        RepoLanguagesComponent
+      ],
+      imports: [ 
+        BrowserAnimationsModule,
+        HttpClientModule,
+        LayoutModule,
+        MatIconModule,
+        MatListModule,
+        MatSidenavModule,
+        MatToolbarModule
+      ],
+      providers: [
+        ReposService
+      ]
     })
     .compileComponents();
   }));
